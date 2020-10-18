1b867f9a8e06cfa018ef8e959839dd608b1e1912
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 16:00:31 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan10/logika/IF10K10119912Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi proses operator logika
+ * 
+ */
+public class IF10K10119912Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
